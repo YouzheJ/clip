396dc020aa124d605b6af94928dcf33d78709a72
youzhej <youzhej@youzhejdeMacBook-Pro.local>
2018-07-11 00:30:33 +0800

    尝试抠图

--- src/demo/js/index.js
+++ src/demo/js/index.js
@@ -21,6 +21,20 @@ var Clipping = function () {
 Clipping.prototype.init = function () {
   this.pattern = this.getBGPattern(this.context);
   this.drawBG();
+  this.getPainter();
+  var that = this;
+  this.drawImg(function () {
+    that.painterContext.globalCompositeOperation = 'destination-out';
+  
+    that.drawRect(that.painterContext, 'rgba(0,0,0,1)', 500, 500, 300, 300);
+    that.context.drawImage(that.painterCanvas, 0, 0, that.width, that.height);
+  });
+
+}
+
+Clipping.prototype.drawRect = function (context, color, startX, startY, width, height) {
+  context.fillStyle = color;
+  context.fillRect(startX, startY, width, height);
 }
 
 // 绘制栅格化背景
@@ -39,19 +53,32 @@ Clipping.prototype.getBGPattern = function (context) {
   return context.createPattern(bgItemCanvas, 'repeat');
 }
 
-Clipping.prototype.drawRect = function (context, color, startX, startY, width, height) {
-  context.fillStyle = color;
-  context.fillRect(startX, startY, width, height);
-}
-
 Clipping.prototype.drawBG = function () {
-  this.context.fillStyle = this.pattern;
-  this.context.fillRect(0, 0, this.width, this.height)
+  this.drawRect(this.context, this.pattern, 0, 0, this.width, this.height)
 }
 
+Clipping.prototype.getPainter = function () {
+  this.painterCanvas = document.createElement('canvas');
+  this.painterCanvas.width = this.width;
+  this.painterCanvas.height = this.height;
+  this.painterContext = this.painterCanvas.getContext('2d');
+}
 
-Clipping.prototype.drawImg = function () {
-  
+Clipping.prototype.getImg = function (url, cb) {
+  var img = new Image();
+  img.src = url;
+  img.onload = function () {
+    cb && cb(img);
+  }
+}
+
+// TODO: 根据图片大小显示，超大按最大显示
+Clipping.prototype.drawImg = function (cb) {
+  var that = this;
+  this.getImg('./images/IMGP3122.jpg', function (img) {
+    that.painterContext.drawImage(img, 0, 0, that.width, that.height);
+    cb && cb();
+  });
 }
 
 var clip = new Clipping();
