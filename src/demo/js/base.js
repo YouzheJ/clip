a7c0b5a7bc1483dcf151e9a25725868c4cf4f081
youzhej <youzhej@youzhejdeMacBook-Pro.local>
2018-07-15 12:12:24 +0800

    base方法第一版

---
+++ src/demo/js/base.js
@@ -0,0 +1,184 @@
+/**
+ * 最基础的类
+ * 提供绘制的方法、事件的绑定、图层创建等
+ */
+var Base = (function () {
+  // 私有变量
+  var layerIdLevelMap = {}; // 记录各图层的id和层级顺序
+  var layerIdList = []; // 记录各图层的id列表，用于新增层时判断id是否重复
+  var layerLevelIterator = 0; // 当前绘制层的id迭代器
+  var eventQueue = {
+    // 事件队列对象，key为各种事件
+    // 图层的层级大的在前面
+    'click': [],
+    'dbclick': [],
+    'rightclick': [],
+    'mouseup': [],
+    'mousedown': [],
+    'mousemove': [],
+    'mouseover': [],
+    'mouseout': [],
+    'mousewheel': [],
+  };
+
+  /**
+   * 生成图层id
+   */
+  function getLayerId () {
+    var id = parseInt(Math.random() * Math.pow(10, 10));
+    if (!~layerIdList.indexOf(id)) return id;
+    return getLayerId();
+  }
+
+  /**
+   * 执行事件队列
+   * @param {string} type 
+   */
+  function executeQueue (type) {
+    return function (e) {
+      var queue = eventQueue[type];
+      if (!queue || !queue.length) return;
+      for (var i = 0, len = queue.length; i < len; i++) {
+        //根据每个事件的返回值决定是否要往下执行
+        // 返回true停止往下走，即阻止冒泡
+        var func = queue[i];
+        if (func && func({
+          x: e.offsetX,
+          y: e.offsetY,
+          originEvent: e
+        })) return;
+      }
+    }
+  }
+
+  var _Base = function (config) {
+    if (!config) config = {};
+    var clientWidth = document.body.clientWidth;
+    var clientHeight = document.body.clientHeight;
+    this.width = config.width || clientWidth;
+    this.height = config.height || clientHeight;
+
+    this.canvas = document.createElement('canvas');
+    this.canvas.id = config.id || 'canvas';
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+
+    // 绘制到真实canvas前，先绘制到虚拟canvas
+    this.preDrawLayer = this.createLayer();
+  }
+
+  /**
+   * 清除所有层，重新绘制前要执行
+   */
+  _Base.prototype.clearLayer = function () {
+    this.canvas.clearRect(0, 0, this.width, this.height);
+  }
+
+  /**
+   * 绘制矩形
+   * @param {context} context canvas的context
+   * @param {string} color 颜色值（rgb|hex）
+   * @param {number} startX 起点横坐标
+   * @param {number} startY 起点纵坐标
+   * @param {number} width 宽
+   * @param {number} height 高
+   */
+  _Base.prototype.drawRect = function (context, color, startX, startY, width, height) {
+    context.fillStyle = color;
+    context.fillRect(startX, startY, width, height);
+  }
+
+  /**
+   * 绘制直线
+   * @param {context} context 
+   * @param {number} startX 
+   * @param {number} startY 
+   * @param {number} endX 
+   * @param {number} endY 
+   */
+  _Base.prototype.drwaLine = function (context, startX, startY, endX, endY) {
+    context.moveTo(startX, startY);
+    context.lineTo(endX, endY);
+  }
+
+  /**
+   * 创建图层
+   * @param {?number} width 
+   * @param {?number} height 
+   */
+  _Base.prototype.createLayer = function (width, height) {
+    var canvas = document.createElement('canvas');
+    canvas.width = width || this.width;
+    canvas.height = height || this.height;
+    var context = canvas.getContext('2d');
+    var layerId = getLayerId();
+    layerIdList.push(layerId);
+    return {
+      canvas: canvas,
+      context: context,
+      id: layerId,
+    }
+  }
+
+  /**
+   * 
+   * @param {array} layerObjs 创建图层时获得的对象数组
+   */
+  _Base.prototype.drawLayer = function (layerObjs) {
+    if (!layerObjs || !layerObjs.length) return;
+    layerLevelIterator = 0;
+    for (var i = 0, len = layerObjs.length; i < len; i++) {
+      layerItem = layerObjs[i];
+      if (!layerItem) continue;
+      layerLevelIterator++;
+      layerIdLevelMap[layerItem.id] = layerLevelIterator;
+      !~layerIdList.indexOf(layerItem.id) && layerIdList.push(layerItem.id);
+      this.preDrawLayer.context.drawImage(layerItem.canvas, 0, 0, this.width, this.height);
+    }
+    this.context.drawImage(this.preDrawLayer.canvas, 0, 0, this.width, this.height);
+  }
+
+  // 添加各种鼠标事件
+  _Base.prototype.addMouseEvent = function () {
+    var element = document.getElementById(this.canvas.id);
+    element.onclick = this.click;
+    // element.onmousedown = this.mouseDown;
+    // element.onmouseup = this.mouseUp;
+    // element.onmousemove = this.mouseMove;
+  }
+
+  /**
+   * 添加事件
+   * @param {object} layerObj 
+   * @param {string} eventType 
+   * @param {function} eventfunc 
+   */
+  _Base.prototype.addListener = function (layerObj, eventType, eventfunc) {
+    var queue = eventQueue[eventType];
+    var level = layerObj.level;
+    var eventItem = {
+      'level': layerObj.level,
+      'id': layerObj.id,
+      'func': eventfunc
+    };
+    // 根据level按序插入
+    if (!queue.length) {
+      queue.push(eventItem);
+      return;
+    }
+    for (var i = 0, len = queue.length; i < len; i++) {
+      if (!queue[i] || queue[i].level < level) {
+        queue.splice(i, 0, eventItem);
+        return;
+      }
+    }
+  }
+
+  /**
+   * 
+   * @param {*} e 
+   */
+  _Base.prototype.click = executeQueue('click');
+
+  return _Base;
+})();
--- src/demo/js/index.js
+++ src/demo/js/index.js
@@ -1,12 +1,20 @@
+/**
+ * 将所有层进行汇总
+ */
 var Clipping = function () {
   var clientWidth = document.body.clientWidth;
   var clientHeight = document.body.clientHeight;
   this.width = clientWidth;
   this.height = clientHeight;
 
+  var baseLayer = new Base();
+
   this.canvas = document.createElement('canvas');
+  this.canvas.id = 'canvas';
   this.canvas.width = this.width;
   this.canvas.height = this.height;
+
+  this.pathList = [];
 
   if (!this.canvas.getContext) {
     alert('您的浏览器不支持canvas！');
@@ -23,7 +31,8 @@ Clipping.prototype.init = function () {
   var layer = this.createLayer();
   this.painterCanvas = layer.canvas;
   this.painterContext = layer.context;
-  this.drawImg(this.clearImgByPath.bind(this));
+  this.addMouseEvent();
+  this.drawImg(this.initPathCanvas.bind(this));
 }
 
 /**
@@ -38,6 +47,11 @@ Clipping.prototype.init = function () {
 Clipping.prototype.drawRect = function (context, color, startX, startY, width, height) {
   context.fillStyle = color;
   context.fillRect(startX, startY, width, height);
+}
+
+Clipping.prototype.drwaLine = function (context, startX, startY, endX, endY) {
+  context.moveTo(startX, startY);
+  context.lineTo(endX, endY);
 }
 
 /**
@@ -95,17 +109,46 @@ Clipping.prototype.drawImg = function (cb) {
   });
 }
 
-Clipping.prototype.clearImgByPath = function () {
-  this.painterContext.globalCompositeOperation = 'destination-out';
-
+Clipping.prototype.initPathCanvas = function () {
+  // this.painterContext.globalCompositeOperation = 'destination-out';
   var layer = this.createLayer();
   this.pathCanvas = layer.canvas;
   this.pathContext = layer.context;
+  this.context.drawImage(this.painterCanvas, 0, 0, this.width, this.height);
+}
+
+Clipping.prototype.clearImgByPath = function () {
   //TODO: 使用鼠标绘制路径
-  this.drawRect(this.pathContext, 'rgba(0,0,0,1)', 500, 500, 300, 300);
+  this.pathContext.beginPath();
+  for (var i = 0, len = this.pathList.length; i < len; i++) {
+    var nextIndex = i === len - 1 ? 0 : i + 1;
+    var currentItem = this.pathList[i], nextItem = this.pathList[nextIndex];
+    this.drwaLine(this.pathContext, currentItem.x, currentItem.y, nextItem.x, nextItem.y);
+  }
+  this.pathContext.strokeStyle = 'red';
+  this.pathContext.stroke();
+  // this.drawRect(this.pathContext, 'rgba(0,0,0,1)', 500, 500, 300, 300);
   this.painterContext.drawImage(this.pathCanvas, 0, 0, this.width, this.height);
 
   this.context.drawImage(this.painterCanvas, 0, 0, this.width, this.height);
+}
+
+// 添加各种鼠标事件
+Clipping.prototype.addMouseEvent = function () {
+  var element = document.getElementById(this.canvas.id);
+  element.onmousedown = this.click.bind(this);
+  // element.onmousedown = this.mouseDown;
+  // element.onmouseup = this.mouseUp;
+  // element.onmousemove = this.mouseMove;
+}
+
+Clipping.prototype.click = function (e) {
+  console.log(e.offsetX, e.offsetY);
+  this.pathList.push({
+    x: e.offsetX,
+    y: e.offsetY
+  });
+  this.clearImgByPath();
 }
 
 var clip = new Clipping();
