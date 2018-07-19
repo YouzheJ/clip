ee582e5e78082d1c6f4fb55d78c28c80ed2eddbf
youzhej <youzhej@youzhejdeMacBook-Pro.local>
2018-07-20 00:37:31 +0800

    可以更新抠图

--- src/demo/js/mask.js
+++ src/demo/js/mask.js
@@ -4,6 +4,7 @@
 
  var Mask = function () {
   this.layer = this.createLayer();
+  this.ctx = this.layer.context;
 }
 
 Mask.prototype = new Base();
@@ -12,14 +13,18 @@ Mask.prototype.draw = function (config) {
   if (!config) config = {};
   var imgLayer = config.imgLayer;
   var pathList = config.pathList;
-  
-  this.layer.context.drawImage(imgLayer.canvas, 0, 0, this.width, this.height);
-  // this.layer.context.globalCompositeOperation = 'destination-out'; // 保留选区外
-  this.layer.context.globalCompositeOperation = 'destination-in'; // 保留选区内
-  this.layer.context.moveTo(pathList[0].x, pathList[0].y);
+  this.ctx.globalCompositeOperation = 'destination-over'; // 恢复正常层叠状态
+  this.ctx.clearRect(0, 0, this.width, this.height);
+  this.ctx.drawImage(imgLayer.canvas, 0, 0, this.width, this.height);
+  // this.ctx.globalCompositeOperation = 'destination-out'; // 保留选区外
+  this.ctx.globalCompositeOperation = 'destination-in'; // 保留选区内
+  this.ctx.beginPath();
+  this.ctx.moveTo(pathList[0].x, pathList[0].y);
   for (var i = 0, len = pathList.length; i < len - 1; i++) {
     var nextItem = pathList[i + 1];
-    this.layer.context.lineTo(nextItem.x, nextItem.y);
+    this.ctx.lineTo(nextItem.x, nextItem.y);
   }
-  this.layer.context.fill();
+  this.ctx.closePath();
+  this.ctx.fillStyle = 'red';
+  this.ctx.fill();
  }
